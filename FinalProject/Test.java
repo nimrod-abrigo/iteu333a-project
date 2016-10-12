db9f9da62757872eb5a02b002013af01e2616644
Nimrod Abrigo <abrigonimrod@gmail.com>
2016-10-13 07:07:47 +0800

    add input.txt reading file and translate to java

---
+++ FinalProject/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args){
+        int i = 0;
+		if(i==1){
+			System.out.println("kumusta mundo");
+		}
+	}
+}
--- FinalProject/src/finalproject/FinalProject.java
+++ FinalProject/src/finalproject/FinalProject.java
@@ -1,10 +1,39 @@
-
 package finalproject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class FinalProject {
-    
-    public static void main(String[] args) {
-       
+
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException, IOException {
+        Scanner sc = new Scanner(new FileReader("input.txt"));
+
+        //reads the file "input.txt"
+        PrintWriter writer = new PrintWriter("Test.java", "UTF-8");
+
+        while (sc.hasNext()) {
+            String str = sc.nextLine();
+            //replaces kung with if
+            str = str.replaceAll("kung", "if");
+            str = str.replaceAll("labas", "out");
+            str = str.replaceAll("iprint", "println");
+
+            writer.println(str);
+        }
+        writer.close();
+        /*List cmdAndArgs = Arrays.asList("cmd", "/c", "run.bat");
+        File dir = new File(System.getProperty("user.dir"));
+
+        ProcessBuilder pb = new ProcessBuilder(cmdAndArgs);
+        pb.directory(dir);
+        Process p = pb.start();*/
     }
-    
+
 }
