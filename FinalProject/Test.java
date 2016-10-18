c58e4f0ab5f4120854f160a9ee8fa7afd7435424
Nimrod Abrigo <abrigonimrod@gmail.com>
2016-10-18 12:21:34 +0800

    sample output

--- FinalProject/Test.java
+++ FinalProject/Test.java
@@ -1,8 +1,12 @@
 public class Test {
-    public static void main(String[] args){
-        int i = 0;
-	String pi = "hi";
-	iprint("kumusta mundo");
-	iprint(pi);
-	}
+public static void main(String[] args){
+int i = 0;
+int j = 0;
+int k = 0;
+String num = "hello world";
+System.out.println("kumusta mundo");
+System.out.println("hahahaah");
+//May syntax error
+printVar(i);
+}
 }
