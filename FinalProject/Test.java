8d69020ce40f7dd56ed4e80c49bc004dc4de198f
Eldz <Eldz@ELDWIN>
2016-10-27 05:21:48 +0800

    parseMDAS

--- FinalProject/Test.java
+++ FinalProject/Test.java
@@ -1,9 +1,9 @@
 public class Test {
 public static void main(String[] args){
-int i = 0;
-int j = 0;
+int i = 1;
+int j = 1;
 int k_1 = 0;
-float average = 99.1131;
+double average = 99.1131;
 
 String num = "hello world";
 String word1 = "Pedro";
@@ -16,11 +16,16 @@ System.out.println(k_1);
 System.out.println(num);
 System.out.println(average);
 
+System.out.println(i + j);
+System.out.println(i - j);
+System.out.println(i/j);
+System.out.println(i * j);
+System.out.println(i % j);
 System.out.println(word1.length());
 String programInput = "test";
 System.out.println(programInput.length());
 
-Concat(word1+word2); //May syntax error
+Concat(word1+word2);
 Concat("Hello"+"World"); //May syntax error
 }
 }
